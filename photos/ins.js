7163efce051a07a4777683d6997221f43ccd1cf5
tangzhihui520 <1776577245@qq.com>
2021-05-19 01:34:00 +0800

    Site updated: 2021-05-19 01:33:57

--- photos/ins.js
+++ photos/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           // 需要修改https://raw.githubusercontent.com/你的github名/你的github仓库名/master/min_photos/即可
-          var minSrc = 'https://raw.githubusercontent.com/tangzhihui520/img/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/tangzhihui520/img/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/tangzhihui520/tangzhihui520.github.io/main/Blog_Album/img/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/tangzhihui520/tangzhihui520.github.io/main/Blog_Album/img/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
